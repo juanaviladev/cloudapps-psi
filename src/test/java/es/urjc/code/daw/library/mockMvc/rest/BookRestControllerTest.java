6fe5ad76c1a5949f2ed7e7d1a0b168e6816b520a
cristoflop <cristofer.lc.99@gmail.com>
2021-01-24 20:54:26 +0100

    Added test case with unauthorized user trying to add new book

--- src/test/java/es/urjc/code/daw/library/mockMvc/rest/BookRestControllerTest.java
+++ src/test/java/es/urjc/code/daw/library/mockMvc/rest/BookRestControllerTest.java
@@ -1,15 +1,16 @@
 package es.urjc.code.daw.library.mockMvc.rest;
 
+import es.urjc.code.daw.library.book.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,15 +21,36 @@ public class BookRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private final String booksUrl = "/api/books/";
+    private Book book;
+
+    @BeforeTestClass
+    public void init() {
+        this.book = new Book("LA GUERRA DE LAS GALAXIAS",
+                "Serie de peliculas de George Lucas donde se narra la vida de los jedi");
+    }
+
     @Test
     public void testGivenNonRegisteredUserWhenGetAllBooksThenCanAccessBookList() throws Exception {
-        String getBooksUrl = "/api/books/";
-
         this.mockMvc.perform(
-                MockMvcRequestBuilders.get(getBooksUrl)
+                MockMvcRequestBuilders.get(this.booksUrl)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(0))));
+    }
+
+    @Test
+    public void testGivenNonRegisteredUserWhenTryToAddNewBookThenReturnStatusForbidden() throws Exception {
+        String addBookUrl = "/api/books/";
+
+        Book book = new Book("LA GUERRA DE LAS GALAXIAS",
+                "Serie de peliculas de George Lucas donde se narra la vida de los jedi");
+
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.post(this.booksUrl, this.book)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+
     }
 
 }
